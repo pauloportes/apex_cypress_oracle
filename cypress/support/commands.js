e9b55dd785378a56e5ed69a89b8f99985e2f894d
Paulo Portes <pauloaportes@gmail.com>
2024-11-08 23:21:58 +0000

    add login test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,12 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const password = Cypress.env('password');
+const username_test = Cypress.env('username_test');
+const base_url = Cypress.env('base_url');
+
+Cypress.Commands.add('login', () => {  
+    cy.session('loginSession', () => {
+    cy.visit(base_url)
+    cy.get('#P9999_USERNAME').type(username_test, { log: false })
+    cy.get('#P9999_PASSWORD').type(password, { log: false })
+    cy.get('#B84609386775701726845').click({ force: true })
+    })  
+});
