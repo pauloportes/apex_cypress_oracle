09c1f57e04f39684447be12d4a316020565e1f3d
Paulo Portes <pauloaportes@gmail.com>
2024-11-08 19:02:08 +0000

    cypress configuration

---
+++ cypress/e2e/apex_market.cy.js
@@ -0,0 +1,17 @@
+/// <reference types= "cypress"/>
+
+
+describe('apex test', () => {
+
+        let user
+
+        before(() => {
+            cy.visit('')
+        })
+
+
+        it ('', () => {
+            cy.get('')
+                .type(``)
+        })
+})
